1b82b47f5827b3a5c17e2405d13e48564d3816c5
hawk153 <hawk153@me.com>
2022-08-22 12:47:30 +0300

    pr01: add ask

--- 100.py
+++ 100.py
@@ -8,3 +8,26 @@ def privetstvie():
     print(" формат ввода: x y ")
     print(" x - номер строки  ")
     print(" y - номер столбца ")
+
+pole = [[" "] * 3 for i in range(3)]
+
+def kartinka():
+    print(f" 0 1 2")
+    print(f"0 {pole[0][0]} {pole[0][1]} {pole[0][2]}")
+    print(f"1 {pole[1][0]} {pole[1][1]} {pole[1][2]}")
+    print(f"2 {pole[2][0]} {pole[2][1]} {pole[2][2]}")
+
+
+def ask():
+    while True:
+        cords = input("         Ваш ход: ").split()
+
+        if len(cords) != 2:
+            print(" Введите 2 координаты! ")
+            continue
+
+        x, y = cords
+
+        if not (x.isdigit()) or not (y.isdigit()):
+            print(" Введите числа! ")
+            continue
